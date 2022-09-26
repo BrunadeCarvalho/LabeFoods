b6a248b22e88552dad4b72194e4c4432daf61c90
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-09-26 13:59:04 -0300

    Botão de remover de acordo com a quantidade está funcionando, layout quando o carrinho está vazio também está funcionando, e criação dos inputs ppara método de pagamento

--- labefood-bruna/src/Pages/MeuCarrinho/MeuCarrinho.js
+++ labefood-bruna/src/Pages/MeuCarrinho/MeuCarrinho.js
@@ -4,7 +4,7 @@ import { BotaoLaranja } from "../../Components/Botoes/styled"
 import { CardItens, InformacaoProduto, Preco } from "../../Components/Cards/Style"
 import {  FooterComponents } from "../../Components/Footer/Footer"
 import { GlobalStateContext } from "../../Global/GlobalStateContext"
-import { DadosRestaurante, DivFundoPaginaFooter, DivValorTotal, Frete, TextoCarrinho } from "./styled"
+import { DadosRestaurante, DivFundoPaginaFooter, DivValorTotal, Frete, PagamentoStyled, TextoCarrinho } from "./styled"
 
 export const MeuCarrinhoPage=(props)=>{
 
@@ -15,7 +15,12 @@ export const MeuCarrinhoPage=(props)=>{
     const deletarProdutos = (produto) =>{
         const novoCarrinho = [...addProduto]
         const deletar = novoCarrinho.findIndex((p)=> p === produto)
-        novoCarrinho.splice(deletar, 1)
+
+        if(novoCarrinho[deletar].quantity===1){
+            novoCarrinho.splice(deletar, 1)
+        }else{
+            novoCarrinho[deletar].quantity--
+        }
         setAddProduto(novoCarrinho)
     }
 
@@ -49,11 +54,16 @@ export const MeuCarrinhoPage=(props)=>{
 
     return(
         <DivFundoPaginaFooter>
+            {carrinho.length > 0 ?
             <DadosRestaurante>
+                <img src={infoRestaurante?.logoUrl} alt={infoRestaurante?.name} />
                 <p className="estabelecimento">{infoRestaurante?.name}</p>
                 <p className="endereco">{infoRestaurante?.address}</p>
                 <p className="entrega">{infoRestaurante?.deliveryTime} min</p>
             </DadosRestaurante>
+            :
+            undefined
+            }
             {carrinho.length > 0 ?
             <DivFundoPaginaFooter>
                 {carrinho}
@@ -61,13 +71,28 @@ export const MeuCarrinhoPage=(props)=>{
             :
             <TextoCarrinho>Carrinho vazio</TextoCarrinho>  
             }
+            {carrinho.length > 0 ? 
             <Frete>
                 <p className="valorFrete">Frete: {infoRestaurante.shipping.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
             </Frete>
+            :
+            <Frete>
+                <p className="valorFrete">Frete: R$ 0,00</p>
+            </Frete>
+            }
             <DivValorTotal>
                 <p className="subtotal">SUBTOTAL:</p> 
                 <p className="valor"> {valorTotal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
             </DivValorTotal>
+            <h6>Forma de pagamento</h6>
+            <PagamentoStyled>
+                <input type="radio"  name="fav_language"/> 
+                <label>Dinheiro</label>
+            </PagamentoStyled>
+            <PagamentoStyled>
+                <input type="radio"  name="fav_language"/> 
+                <label>Cartão de crédito</label>
+            </PagamentoStyled>
             <BotaoLaranja>Confirmar</BotaoLaranja>
             <FooterComponents />
         </DivFundoPaginaFooter>
